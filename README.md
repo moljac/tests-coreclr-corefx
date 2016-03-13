98beedd48db64abea9e39c2d0f2c1a4b19e2158e
moljac <mcvjetko@holisticware.net>
2016-03-13 20:45:10 +0100

    solaris pkg install script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tests-coreclr-corefx
+
 tests-coreclr-corefx
+
+*	[https://github.com/dotnet/coreclr](https://github.com/dotnet/coreclr)
+
---
+++ git/bat/solaris/install-erequisites.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+
+# Solaris pkg command 
+#	http://www.bolthole.com/solaris/solaris11-pkg.html
+# 	http://www.pearsonitcertification.com/articles/article.aspx?p=31540&seqNum=3
+
+TOOLS=\
+	git \
+	autoconf \
+	automake \
+	gnu-make \
+	make \
+	cmake \
+	gcc4core \
+	gcc4g++ \
+	gcc5core \
+	gcc5g++ \
+	gdb \
+	
+
+for PACKAGE in $PACKAGES;
+do
+	echo $PACKAGE
+done
+
+	
+#	PACKAGE=vim	
+#	pkgadd -d http://get.opencsw.org/now
+#	/opt/csw/bin/pkgutil -U
+#	/opt/csw/bin/pkgutil -a $PACKAGE
+#	/opt/csw/bin/pkgutil -y -i $PACKAGE
+	
